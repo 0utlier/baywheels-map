5db5bd50ca65a9bf109dfecc67d5afd85a7859cb
0utlier <jdsqrpnts@yahoo.com>
2025-03-13 14:46:33 -0700

    trying apple again

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ folium_static(folium_map)
 # Add icon libraries using markdown
 st.markdown("""
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 """, unsafe_allow_html=True)
 
