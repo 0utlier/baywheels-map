7ce8435f7055aa07a862ba6fc836b57bb4bd3827
0utlier <jdsqrpnts@yahoo.com>
2025-03-13 15:00:02 -0700

    move order

--- app.py
+++ app.py
@@ -109,21 +109,21 @@ st.markdown("""
 col1, col2 = st.columns(2)
 
 with col1:
-    # Android button
-    st.markdown("""
-        <a href="intent://#Intent;scheme=baywheels;package=com.motivateco.gobike;end" target="_blank">
-            <button style="padding:10px 20px; background-color:#28A745; color:white; border:none; border-radius:5px; cursor:pointer; display:inline-block;">
-                <i class="material-icons">android</i>
-            </button>
-        </a>
-    """, unsafe_allow_html=True)
-
-with col2:
     # iPhone button
     st.markdown("""
         <a href="https://apps.apple.com/us/app/bay-wheels/id1233398899" target="_blank">
             <button style="padding:10px 20px; background-color:#007BFF; color:white; border:none; border-radius:5px; cursor:pointer; display:inline-block;">
                 <i class="fab fa-app-store-ios"></i>
+            </button>
+        </a>
+    """, unsafe_allow_html=True)
+
+with col2:
+    # Android button
+    st.markdown("""
+        <a href="intent://#Intent;scheme=baywheels;package=com.motivateco.gobike;end" target="_blank">
+            <button style="padding:10px 20px; background-color:#28A745; color:white; border:none; border-radius:5px; cursor:pointer; display:inline-block;">
+                <i class="material-icons">android</i>
             </button>
         </a>
     """, unsafe_allow_html=True)
